eb4cb0345469270c04abd73005ff044645d5f250
Noah <nordqvist.gaming@gmail.com>
2024-07-01 19:45:16 +0200

    tempo rary fix invalid or corrupt scene bug

--- voxel.gd
+++ voxel.gd
@@ -1,13 +1,17 @@
 extends RigidBody3D
 
+var voxel_scene: PackedScene
+var holo_voxel_scene: PackedScene
 var holo_voxel_instance: Node = null
 
-@onready var voxel_scene: PackedScene = preload("res://voxel.tscn")
-@onready var holo_voxel_scene: PackedScene = preload("res://holo_voxel.tscn")
+# @onready var voxel_scene: PackedScene = preload("res://voxel.tscn")
+# @onready var holo_voxel_scene: PackedScene = preload("res://HoloVoxel.tscn")
 
 
 func _ready() -> void:
 	input_event.connect(on_voxel_input_event)
+	voxel_scene = preload("res://voxel.tscn")
+	holo_voxel_scene = preload("res://holo_voxel.tscn")
 
 
 func on_voxel_input_event(camera: Node, event: InputEvent, click_position: Vector3, normal: Vector3, shape_idx: int) -> void:
