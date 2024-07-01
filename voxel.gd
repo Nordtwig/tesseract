23d56677dc43101a84ebe54d4ffe7b560d2d244b
Noah <nordqvist.gaming@gmail.com>
2024-07-01 20:30:23 +0200

    refactor hover voxel indicator

--- voxel.gd
+++ voxel.gd
@@ -1,8 +1,10 @@
 extends RigidBody3D
 
-var voxel_scene: PackedScene
-var holo_voxel_scene: PackedScene
-var holo_voxel_instance: Node = null
+@export var voxel_scene: Resource
+@export var mesh_sides: Array[MeshInstance3D]
+
+var placeholder_voxel_instance: Node = null
+var mouse_is_over_voxel: bool = false
 
 # @onready var voxel_scene: PackedScene = preload("res://voxel.tscn")
 # @onready var holo_voxel_scene: PackedScene = preload("res://HoloVoxel.tscn")
@@ -10,19 +12,37 @@ var holo_voxel_instance: Node = null
 
 func _ready() -> void:
 	input_event.connect(on_voxel_input_event)
-	voxel_scene = preload("res://voxel.tscn")
-	holo_voxel_scene = preload("res://holo_voxel.tscn")
+	mouse_entered.connect(on_voxel_mouse_entered)
+	mouse_exited.connect(on_voxel_mouse_exited)
+	voxel_scene = load("res://voxel.tscn")
+	# holo_voxel_scene = preload("res://holo_voxel.tscn")
 
+
+func set_material(material: String) -> void:
+	for mesh in mesh_sides:
+		mesh.material_override = load(material)
 
 func on_voxel_input_event(camera: Node, event: InputEvent, click_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	print(mouse_is_over_voxel)
 	if event.is_action_pressed("left_click"):
-		var voxel_instance = voxel_scene.instantiate()
-		voxel_instance.set_script(get_script())
-		get_tree().get_root().get_node("Main").add_child(voxel_instance)
-		voxel_instance.position = position + (normal * Game.VOXEL_SIZE)
+		placeholder_voxel_instance.set_material("res://voxel.tres")
+		placeholder_voxel_instance.get_node("CollisionShape3D").disabled = false
+		placeholder_voxel_instance = null
 	elif event is InputEventMouseMotion:
-		if !holo_voxel_instance:
-			holo_voxel_instance = holo_voxel_scene.instantiate()
-			holo_voxel_instance.set_script(get_script())
-			get_tree().get_root().get_node("Main").add_child(holo_voxel_instance)
-			holo_voxel_instance.position = position + (normal * Game.VOXEL_SIZE)
+		if !placeholder_voxel_instance and mouse_is_over_voxel:
+			placeholder_voxel_instance = voxel_scene.instantiate()
+			placeholder_voxel_instance.set_script(get_script())
+			get_tree().get_root().get_node("Main").add_child(placeholder_voxel_instance)
+			placeholder_voxel_instance.set_material("res://holo_voxel.tres")
+			placeholder_voxel_instance.get_node("CollisionShape3D").disabled = true
+			placeholder_voxel_instance.position = position + (normal * Game.VOXEL_SIZE)
+
+
+func on_voxel_mouse_entered() -> void:
+	mouse_is_over_voxel = true
+
+
+func on_voxel_mouse_exited() -> void:
+	if placeholder_voxel_instance != null:
+		placeholder_voxel_instance.queue_free()
+	mouse_is_over_voxel = false
