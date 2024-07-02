4ee2a1e04599d06d96f5650f3f93ac9582c06ccf
Noah <nordqvist.gaming@gmail.com>
2024-07-02 13:36:14 +0200

    implement simple zoom

--- camera_pole.gd
+++ camera_pole.gd
@@ -5,12 +5,15 @@ var move_speed: float = 1.0
 var speed_modifier: float = 2.0
 var should_move_to_new_position: bool = false
 var forward = Vector3.FORWARD
+var scroll_speed: float = 5.0
+var camera: Camera3D
 
 @onready var pivot: Node3D = $Pivot
 
 
 func _ready() -> void:
 	Events.voxel_right_clicked.connect(on_voxel_right_clicked)
+	camera = pivot.get_node("Camera")
 
 
 func _process(delta) -> void:
@@ -18,6 +21,11 @@ func _process(delta) -> void:
 		pivot.rotation.x +=  MouseInput.relative.y * delta
 		pivot.rotation.x = clampf(pivot.rotation.x, -1.6, 1.6)
 		pivot.rotation.y += MouseInput.relative.x * delta
+	if Input.is_action_just_pressed("scroll_forward"):
+		camera.position.z -= scroll_speed * delta
+	elif Input.is_action_just_pressed("scroll_backward"):
+		camera.position.z += scroll_speed * delta
+	camera.position.z = clamp(camera.position.z, 0.5, 10)
 
 
 func _physics_process(delta) -> void:
@@ -44,9 +52,7 @@ func get_input() -> void:
 	if direction != Vector3.ZERO:
 		should_move_to_new_position = false
 		direction.y = position.y
-	print(direction)
 	velocity = direction * move_speed
-	print(velocity)
 
 
 func on_voxel_right_clicked(voxel_position: Vector3) -> void:
